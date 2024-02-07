8ea51ac01d5f7a7c426f204ae6ce8184e64e666e
Ricwolf19 <rhtc19@gmail.com>
2024-02-06 22:13:20 -0600

    dashboard on the road

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,9 +1,9 @@
-//Import Icons
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'remixicon/fonts/remixicon.css'
+
 
 export function Dashboard() {
     return (
-        <div></div>
+        <div className="bg-blue-100">
+            
+        </div>
     )
 }
