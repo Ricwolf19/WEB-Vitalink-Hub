35670340a2febb5ea3ddb750f446b8f3dac012c6
Ricwolf19 <rhtc19@gmail.com>
2024-02-21 16:34:20 -0600

    why vercel throw errors?

--- src/Components/Dashboard/Layout/dashboard-navbar.tsx
+++ src/Components/Dashboard/Layout/dashboard-navbar.tsx
@@ -13,16 +13,10 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 import {
-  UserCircleIcon,
-  // Cog6ToothIcon,
-  BellIcon,
   ClockIcon,
   CreditCardIcon,
-  Bars3Icon,
   ChevronDownIcon,
   Cog6ToothIcon,
-  InboxArrowDownIcon,
-  LifebuoyIcon,
   PowerIcon,
 } from "@heroicons/react/24/solid";
 import {
@@ -67,7 +61,7 @@ function ProfileMenu() {
             size="sm"
             alt="tania andrew"
             className="border border-gray-900 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+            src="/user-icon.jpg"
           />
           <ChevronDownIcon
             strokeWidth={2.5}
